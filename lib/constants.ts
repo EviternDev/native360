81a58ba022ee4d793a6a30b1a15766698eab249b
Sangeeth R <47264999+DRIRU@users.noreply.github.com>
2026-03-14 21:45:19 +0530

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,2 @@
-const WHATSAPP_PRESET_MESSAGE =
-  "Hello Native360,\n\nI'm interested in your premium management and Concierge services.\n\nPlease contact me soon.";
-
+const WHATSAPP_PRESET_MESSAGE = ​"Hello Native 360. I am interested in learning more about your premium management and concierge services for my family and property in Kerala. I would like to schedule an introductory consultation.";
 export const WHATSAPP_BOOK_NOW_URL = `https://wa.me/918848748851?text=${encodeURIComponent(WHATSAPP_PRESET_MESSAGE)}`;
